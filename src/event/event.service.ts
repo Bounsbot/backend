3b24b486fbce14f6f89831d1a2300c1337e7b60e
Fabien Bounoir <71253960+FabienBounoir@users.noreply.github.com>
2023-11-26 22:13:09 +0100

    refactor: 💡 Remove unused element

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,22 +1,8 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { Namespace } from 'socket.io';
-import { EmitPixelwarDto, NotificationType } from './dto/emit-pixelwar.dto';
-import { SchedulerRegistry } from '@nestjs/schedule';
-import { createCanvas, loadImage } from 'canvas';
-import { CACHE_MANAGER, CacheStore } from '@nestjs/cache-manager';
 
 @Injectable()
 export class EventService {
   public constructor(
-    private schedulerRegistry: SchedulerRegistry,
-    @Inject(CACHE_MANAGER) private cacheManager: CacheStore,
-  ) {}
+  ) { }
 
-  async pixelwar(
-    emitPixelwarDto: EmitPixelwarDto,
-    size: number[],
-    server: Namespace,
-  ) {
-
-  }
 }
